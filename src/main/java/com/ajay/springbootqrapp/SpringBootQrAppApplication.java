34630b5acf1a14cef70dbb3f070e6292bf47af7a
Ajay Kumar Verma <ajayvermaakv4@gmail.com>
2021-01-04 20:41:17 +0530

    my first commit

---
+++ src/main/java/com/ajay/springbootqrapp/SpringBootQrAppApplication.java
@@ -0,0 +1,13 @@
+package com.ajay.springbootqrapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootQrAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootQrAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ajay/springbootqrapp/SpringBootQrAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajay.springbootqrapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootQrAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
